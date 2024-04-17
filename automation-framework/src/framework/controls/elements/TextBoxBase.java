c05f9fc6582e863fecaa3f199d5f784494838d33
Furkan Yıldırım <yildirim.frkn@outlook.com>
2024-04-17 14:48:26 +0300

    update TextBoxBase

--- automation-framework/src/framework/controls/elements/TextBoxBase.java
+++ automation-framework/src/framework/controls/elements/TextBoxBase.java
@@ -1,15 +1,21 @@
 package framework.controls.elements;
 
 import framework.controls.internals.ControlBase;
+import org.openqa.selenium.WebElement;
 
 public class TextBoxBase extends ControlBase implements TextBox {
+    public TextBoxBase(WebElement element) {
+        super(element);
+    }
+
     @Override
     public void EnterText(String text) {
 
+        getWrappedElement().sendKeys(text);
     }
 
     @Override
     public String GetTextValue() {
-        return null;
+        return getWrappedElement().getText();
     }
 }
